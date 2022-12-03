c2980311e652d35815d4f2ce38dd7be28e130dd0
derekglewis <105102055+derekglewis@users.noreply.github.com>
2022-12-03 15:46:42 -0500

    V3 Merge Sort Test
    Program was altered to add Merge Sort function

--- IT481M4_SortingAlgorithmTest/IT481M4_SortingAlgorithmTest/Program.cs
+++ IT481M4_SortingAlgorithmTest/IT481M4_SortingAlgorithmTest/Program.cs
@@ -9,43 +9,177 @@ namespace SortingTest
         {
             //////////////////////////////////////////////////////
             ///IT481M4 Algorithms for Analysis and Optimization///
-            //////////////////V1 Bubble Sort//////////////////////
+            ///////////////////V3 Merge Sort//////////////////////
             //////////////////////////////////////////////////////
 
-            ///Note: The term Bubble Sort was first used in 1962 by Iverson and is also known as Exchange Sorting. Average Complexity = O(n^2) http://www.iiitdm.ac.in/old/Faculty_Teaching/Sadagopan/pdf/DAA/SortingAlgorithms.pdf
+            ///Note: Merge Sort was created by John Von Neumann in 1945 as a divide and conquer sorting method with an average complexity of O(n log(n)) https://developer.nvidia.com/blog/merge-sort-explained-a-data-scientists-algorithm-guide/
 
-            //Bubble Sort Execution Time for Small Data Set
+            //Merge Sort Execution Time for Small Data Set
             int[] smallDataSet = DataSets.smallDataSet;
             var executionTimeSmall = System.Diagnostics.Stopwatch.StartNew();
-            BubbleSort(smallDataSet);
+            MergeSort(smallDataSet);
             executionTimeSmall.Stop();
 
-            //Bubble Sort Execution Time for Medium Data Set
+            
+            //Merge Sort Execution Time for Medium Data Set
             int[] medDataSet = DataSets.medDataSet;
             var executionTimeMed = System.Diagnostics.Stopwatch.StartNew();
-            BubbleSort(medDataSet);
+            MergeSort(medDataSet);
             executionTimeMed.Stop();
-
-            //Bubble Sort Execution Time for Large Data Set
+          
+            //Merge Sort Execution Time for Large Data Set
             int[] lrgDataSet = DataSets.lrgDataSet;
             var executionTimeLrg = System.Diagnostics.Stopwatch.StartNew();
-            BubbleSort(lrgDataSet);
+            MergeSort(lrgDataSet);
             executionTimeLrg.Stop();
+            
 
             //Print Results
             Console.ForegroundColor = ConsoleColor.Cyan;
             Console.WriteLine("###################################\n" +
-                "### Bubble Sort Testing Results ###\n" +
+                "### Merge Sort Testing Results ####\n" +
                 "###### Elapsed Time in Ticks ######\n" +
                 "###################################");
             Console.ForegroundColor = ConsoleColor.Yellow;
             Console.WriteLine("{0,-5} {1,7}", "Small Data Set:", executionTimeSmall.ElapsedTicks);
             Console.WriteLine("{0,-5} {1,0}", "Medium Data Set:", executionTimeMed.ElapsedTicks);
-            Console.WriteLine("{0,-5} {1,10}", "Large Data Set:", executionTimeLrg.ElapsedTicks);
+            Console.WriteLine("{0,-5} {1,8}", "Large Data Set:", executionTimeLrg.ElapsedTicks);
             Console.ResetColor();
 
         }
 
+        //Merge Sort Algorithm
+        public static int[] MergeSort(int[] array)
+        {
+            int[] left;
+            int[] right;
+            int[] result = new int[array.Length];
+
+            if (array.Length <= 1)
+                return array;
+            int middle = array.Length / 2;
+            left = new int[middle];
+
+            if (array.Length % 2 == 0)
+                right = new int[middle];
+            else
+                right = new int[middle + 1];
+
+            for (int i = 0; i < middle; i++)
+                left[i] = array[i];
+
+            int j = 0;
+
+            for (int i = middle; i < array.Length; i++)
+            {
+                right[j] = array[i];
+                j++;
+            }
+
+            left = MergeSort(left);
+            right = MergeSort(right);
+            result = MergeArray(left, right);
+            return result;
+
+        }
+
+        public static int[] MergeArray(int[] left, int[] right)
+        {
+            int resultLength = right.Length + left.Length;
+            int[] result = new int[resultLength];
+            int indexLeft = 0, indexRight = 0, indexResult = 0;
+
+            while (indexLeft < left.Length || indexRight < right.Length)
+            {
+                if (indexLeft < left.Length && indexRight < right.Length)
+                {
+                    if (left[indexLeft] <= right[indexRight])
+                    {
+                        result[indexResult] = left[indexLeft];
+                        indexLeft++;
+                        indexResult++;
+                    }
+
+                    else
+                    {
+                        result[indexResult] = right[indexRight];
+                        indexRight++;
+                        indexResult++;
+                    }
+                }
+
+                else if (indexLeft < left.Length)
+                {
+                    result[indexResult] = left[indexLeft];
+                    indexLeft++;
+                    indexResult++;
+                }
+
+                else if (indexRight < right.Length)
+                {
+                    result[indexResult] = right[indexRight];
+                    indexRight++;
+                    indexResult++;
+                }
+            }
+            return result;
+        }
+
+
+
+
+
+
+
+
+
+
+        /*
+        //Quick Sort Algorithm
+        public static void QuickSort(int[] array, int leftIndex, int rightIndex)
+        {
+            var i = leftIndex;
+            var j = rightIndex;
+            var pivot = array[leftIndex];
+
+            while (i <= j)
+            {
+                while (array[i] < pivot)
+                {
+                    i++;
+                }
+
+                while (array[j] > pivot)
+                {
+                    j--;
+                }
+
+                if (i <= j)
+                {
+                    int temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
+                    i++;
+                    j--;
+                }
+            }
+
+            if (leftIndex < j)
+                QuickSort(array, leftIndex, j);
+
+            if (i < rightIndex)
+                QuickSort(array, i, rightIndex);
+
+
+        }
+        */
+
+
+
+
+
+
+        /*
         //Bubble Sort Algorithm
         public static void BubbleSort(int[] array)
         {
@@ -59,17 +193,10 @@ namespace SortingTest
                         array[j] = array[j + 1];
                         array[j + 1] = tempVar;
                     }
+        */
 
-            //Sort Success Check
-            /*for (int i = 0; i < array.Length; i++)
-            {
-                Console.Write($" ==> {array[i]}");
-            }
-            Console.WriteLine("\n");
-            */
-
-        }
 
     }
 }
+
 
